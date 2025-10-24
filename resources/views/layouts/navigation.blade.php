dc0441ecb6cee1925c8d5b1baaacf23cc059883f
Ferdi Yansah <frdynsh11@gmail.com>
2025-10-24 22:30:40 +0700

    TUGAS PERTEMUAN 7

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,46 +15,10 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('product-index')" :active="request()->routeIs('product-index')">
+                        {{ __('Produk') }}
+                    </x-nav-link>
                 </div>
-            </div>
-
-            <!-- PERTEMUAN 5 -->
-            <!-- Data Master Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <x-dropdown align="right" width="48">
-                    <!-- Trigger Button -->
-                    <x-slot name="trigger">
-                        <button
-                            class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 
-                                transition duration-150 ease-in-out bg-white border border-transparent rounded-md 
-                                hover:text-gray-700 focus:outline-none"
-                        >
-                            <div>Data Master</div>
-                            <div class="ms-1">
-                                <svg
-                                    class="w-4 h-4 fill-current"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 20 20"
-                                >
-                                    <path
-                                        fill-rule="evenodd"
-                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 
-                                        1 0 111.414 1.414l-4 4a1 1 0 
-                                        01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                        clip-rule="evenodd"
-                                    />
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
-
-                    <!-- Dropdown Content -->
-                    <x-slot name="content">
-                        <x-dropdown-link :href="route('product-index')">
-                            Product Master
-                        </x-dropdown-link>
-                    </x-slot>
-                </x-dropdown>
             </div>
 
             <!-- Settings Dropdown -->
@@ -108,6 +72,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('product-index')" :active="request()->routeIs('product-index')">
+                {{ __('produk') }}
             </x-responsive-nav-link>
         </div>
 
